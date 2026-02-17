edc4592d23e2c3bbb2c2f2e12da6b05bd89050a4
annVs07 <anastasiiaorl17@gmail.com>
2026-02-17 18:48:41 +0100

    test 2

--- PI2-Projects/src/BinaryAdder.java
+++ PI2-Projects/src/BinaryAdder.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 import java.lang.String;
 
 //Gruppennummer: 109
-//ich mach was
+//ich mach was 22
 public class BinaryAdder {
 	public static String add(String binary1, String binary2) {
 		if(binary1 == null || binary2 == null) {
