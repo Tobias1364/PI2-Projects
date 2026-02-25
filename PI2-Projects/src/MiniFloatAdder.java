0560dc7c6de014bf21add55a4b1a1becc69bbbe8
annVs07 <anastasiiaorl17@gmail.com>
2026-02-25 01:27:05 +0100

    Adding the code file

---
+++ PI2-Projects/src/MiniFloatAdder.java
@@ -0,0 +1,19 @@
+
+public class MiniFloatAdder {
+	/** prints all MiniFloat values in decreasing order */
+	public static void printAllMiniFloatValues() {
+        // YOUR_CODE_HERE
+	}
+
+	/** MiniFloat to double */
+	public static final double toDouble(byte miniF) {
+        // YOUR_CODE_HERE
+		return 0;
+	}
+
+	/** adds two non-negative MiniFloats */
+	public static final byte addMiniFloats(byte miniF1, byte miniF2) {
+        // YOUR_CODE_HERE
+		return 0;
+	}	
+}
